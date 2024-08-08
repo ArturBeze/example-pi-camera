4b35ca5d9cb1ae9ada45b61cb49c558832327999
ArturBeze <ArchiBeze@icloud.com>
2024-08-08 23:26:12 +0300

    update

--- clue/SaveToFile.py
+++ clue/SaveToFile.py
@@ -1,1 +1,23 @@
+import time
+from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder
 
+picam2 = Picamera2()
+
+video_config = picam2.create_video_configuration(main={"size": (1640, 1232)}, \
+	controls={"FrameDurationLimits": (40000, 40000)}, \
+	lores={"size": (640, 480)})
+
+picam2.configure(video_config)
+
+encoder = H264Encoder(10000000)
+output = "test.h264"
+
+picam2.start_preview(Preview.QTGL)
+picam2.start_recording(encoder, output)
+time.sleep(10)
+picam2.stop_recording()
+picam2.stop_preview()
+
+#picam2.capture_metadata()["FrameDuration"]
+#picam2.capture_metadata()["SensorTimestamp"]
--- clue/StreamPiCam.py
+++ clue/StreamPiCam.py
@@ -1,1 +1,21 @@
+import time, libcamera
+from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder
 
+picam2 = Picamera2()
+
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, \
+	lores={"size": (640, 480)}, display="lores")
+
+preview_config = picam2.create_preview_configuration(main={"size": (1600, 1200), "format": "XRGB8888"}, \
+	lores={"size": (640,480), "format": "YUV420"}, display = "lores")
+preview_config["transform"] = libcamera.Transform(hflip = 1, vflip = 1)
+
+picam2.configure(preview_config)
+
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(10)
+picam2.capture_file("test.jpg")
+picam2.stop_preview()
+picam2.close()
